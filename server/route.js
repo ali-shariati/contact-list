f348885bc6ec0be08537311e3f1dd9d029d2a4e6
ali-shariati <seyed.ali.shariati@gmail.com>
2025-11-01 17:14:32 +0330

    feat:add create

--- server/route.js
+++ server/route.js
@@ -1,6 +1,5 @@
 import express from "express";
-import {formatContactList, generateContactId, loadContactList, saveContactList} from "../services.js";
-// import {query} from "../db.js"
+import {formatContactList, loadContactList, saveContactList} from "../services.js";
 import {Contact} from "../models/index.js"
 
 const router = express.Router();
@@ -9,7 +8,7 @@ const contactList = [];
 router.get("/list", async (req, res) => {
 
     try{
-         const contacts = await Contact.findAll()
+        const contacts = await Contact.findAll()
         if(req.query.format){
             const responseData = `<pre>${formatContactList(contactList)}</pre>`;
             res.type('html');
@@ -18,23 +17,28 @@ router.get("/list", async (req, res) => {
         }
         res.json(contacts);
     }catch(err){
-       res.status(500).send({message: "Server error", err});
+        res.status(500).send({message: "Server error", err});
     }
 
 })
 
-router.post("/create", (req, res) => {
-    const {firstName, lastName} = req.body;
-    const id =  generateContactId(contactList);
-
-    const newContact = {
-        id,
-        firstName ,
-        lastName,
+router.post("/create", async (req, res) => {
+    const {firstName, lastName, mobileNumber, isFavorite } = req.body;
+    try {
+        const {id} =  await Contact.create({
+            firstName,
+            lastName,
+            mobileNumber,
+            isFavorite,
+        })
+        res.send(`The Contact "${id} ${firstName} ${lastName}" has been added successfully.`);
+    }catch(err){
+        res.status(400).send({
+                message: "Something went wrong",
+                err
+            }
+        );
     }
-    contactList.push(newContact)
-    saveContactList(contactList);
-    res.send(`The Contact ${firstName} ${lastName} has been added successfully.`);
 });
 
 router.delete("/:id", (req, res) => {
